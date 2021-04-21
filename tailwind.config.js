3300d99457d98d36f215def13ed654505e946958
Benjamin-ochieng <ochiengbenja@gmail.com>
2021-04-21 16:00:32 +0300

    buid(config): Set up the project folders, dev configurations and initial dependancies

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
